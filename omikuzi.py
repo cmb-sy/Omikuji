2fd34b9f3d58d2af09048130194c24b7b3bc9d3e
nakashima1125 <free.nakashima@gmail.com>
2020-12-19 21:46:00 +0900

    modfied if code

---
+++ omikuzi.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import glob
+from time import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    img_path = glob.glob("static/omikuzi/*.jpg")
+    return render_template('index.html', img=img_path[int(time()) % 9])
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
