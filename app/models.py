0b674b90af79b87db738ec34a16aec3c8cc5ef34
nakashima1125 <free.nakashima@gmail.com>
2020-12-21 14:03:26 +0900

    remake view

--- app/models.py
+++ app/models.py
@@ -3,12 +3,9 @@ from app import db
 class OmikuziContent(db.Model):
     __tablename__ = 'OmikuziContent'
     id = db.Column(db.Integer, primary_key=True)
+    main_title = db.Column(db.Text)
     title = db.Column(db.Text)
     text = db.Column(db.Text)
-
-    def __repr__(self):
-        return '<Entry id={id} title={title!r}>'.format(
-                id=self.id, title=self.title)
 
 def init():
     db.create_all()
--- app/views.py
+++ app/views.py
@@ -3,6 +3,11 @@ from flask import Flask
 from flask import request, render_template
 from app.models import OmikuziContent
 from app import db, app
+from flask import (
+    render_template, request,
+    abort, redirect, url_for,
+    flash
+)
 
 omikuzi_list = ["大吉","中吉","小吉","吉","半吉","末吉","凶","大凶"]
 
@@ -19,21 +24,19 @@ def index():
 
 @app.route("/add",methods=["POST"])
 def post():
-    title = request.form["title"]
-    body = request.form["body"]
+    main_title = request.form["title"]
     omikuzicontent = OmikuziContent()
-    omikuzicontent.title = title
-    omikuzicontent.body = body
+    omikuzicontent.main_title = main_title
     db.session.add(omikuzicontent)
     db.session.commit()
-    return self_omikuzi()
+    return redirect(url_for('self_omikuzi'))
 
 @app.route("/self_omikuzi",methods=["POST","GET"])
 def self_omikuzi():
-    omikuzi = OmikuziContent.query.all()
+    omikuzicontent = OmikuziContent()
+    omikuzi_main_title = omikuzicontent.main_title
     if request.method == "POST":
-        b = random.choice(omikuzi)
-        return render_template("self-result.html", b=b)
+        return render_template("self-result.html", omikuzi_title=omikuzi_main_title)
     return 'bad request!', 400
 
 if __name__ == "__main__":
