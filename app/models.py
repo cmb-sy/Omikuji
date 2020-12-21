237511c65c1b89e3d043d9d973a53e4f5b10009d
nakashima1125 <free.nakashima@gmail.com>
2020-12-21 21:42:45 +0900

    add

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
 from app import db
+from sqlalchemy.orm import relationship
+
+class OmikuziTitle(db.Model):
+    __tablename__ = 'OmikuziTitle'
+    id = db.Column(db.Integer, primary_key=True)
+    main_title = db.Column(db.Text)
+    OmikuziContent = relationship("OmikuziContent", backref="OmikuziTitle")
 
 class OmikuziContent(db.Model):
     __tablename__ = 'OmikuziContent'
-    id = db.Column(db.Integer, primary_key=True)
-    main_title = db.Column(db.Text)
+    main_title_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
-    text = db.Column(db.Text)
+    content = db.Column(db.Text)
+    omikuzi_main_title = relationship("OmikuziTitle")
 
 def init():
     db.create_all()
--- app/views.py
+++ app/views.py
@@ -1,13 +1,9 @@
 import random
-from flask import Flask
-from flask import request, render_template
-from app.models import OmikuziContent
+from app.models import OmikuziTitle
 from app import db, app
 from flask import (
     render_template, request,
-    abort, redirect, url_for,
-    flash
-)
+    abort, redirect, url_for,flash, Flask)
 
 omikuzi_list = ["大吉","中吉","小吉","吉","半吉","末吉","凶","大凶"]
 
@@ -22,22 +18,23 @@ def index():
         return render_template("result.html",a=a)
     return render_template("index.html")
 
-@app.route("/add",methods=["POST"])
+@app.route("/add",methods=["POST","GET"])
 def post():
     main_title = request.form["title"]
-    omikuzicontent = OmikuziContent()
-    omikuzicontent.main_title = main_title
-    db.session.add(omikuzicontent)
+    omikuzititle = OmikuziTitle()
+    omikuzititle.main_title = main_title
+    db.session.add(omikuzititle)
     db.session.commit()
     return redirect(url_for('self_omikuzi'))
 
 @app.route("/self_omikuzi",methods=["POST","GET"])
 def self_omikuzi():
-    omikuzicontent = OmikuziContent()
-    omikuzi_main_title = omikuzicontent.main_title
-    if request.method == "POST":
-        return render_template("self-result.html", omikuzi_title=omikuzi_main_title)
-    return 'bad request!', 400
+    if request.method == "GET":
+        #POSTならエラー
+        omikuzititle = OmikuziTitle()
+        omikuzi_main_title = omikuzititle.main_title
+        return render_template("self.html", omikuzi_title=omikuzi_main_title)
+    return render_template("self.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
